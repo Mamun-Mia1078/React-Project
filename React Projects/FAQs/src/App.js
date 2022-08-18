d40c9812c77c18ccc3cb160c40b6f0a17f016f68
Mamun Mia <70141554+Mamun-Mia1078@users.noreply.github.com>
2022-08-18 10:04:51 +0600

    Freequently Answaring Questions app

---
+++ React Projects/FAQs/src/App.js
@@ -0,0 +1,10 @@
+import {FAQS}  from './components/FAQ/FAQS.js';
+import React from "react"
+
+function App(){
+  return <div>
+   
+    <FAQS/>
+  </div>
+}
+export default App;
---
+++ React Projects/FAQs/src/components/FAQ/data.js
@@ -0,0 +1,21 @@
+export const faqsData = [
+    {
+        id: 1 , 
+        title: "How can we reach you ?", 
+        desc: " The national flag of India is the symbol of freedom and unity of the country. It is rectangular. It is also called 'tricolor' or 'tiranaga' as it has three colours. The topmost colour is saffron, white is in the middle, and the lowermost colour is green ", 
+    }, 
+
+    {
+        id: 2 , 
+        title: "When you are available ?", 
+        desc: " The national flag of India is the symbol of freedom and unity of the country. It is rectangular. It is also called 'tricolor' or 'tiranaga' as it has three colours. The topmost colour is saffron, white is in the middle, and the lowermost colour is green ", 
+    }, 
+
+
+
+      {
+        id: 3 , 
+        title: "Where we can find the videos ?", 
+        desc: " The national flag of India is the symbol of freedom and unity of the country. It is rectangular. It is also called 'tricolor' or 'tiranaga' as it has three colours. The topmost colour is saffron, white is in the middle, and the lowermost colour is green ", 
+    }, 
+]
