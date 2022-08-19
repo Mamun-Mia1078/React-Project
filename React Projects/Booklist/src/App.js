2b074c4c1fc7f932dc7a90dbcbeba2956e4b04d7
Mamun Mia <70141554+Mamun-Mia1078@users.noreply.github.com>
2022-08-19 22:40:26 +0600

    booklist

---
+++ React Projects/Booklist/src/App.js
@@ -0,0 +1,9 @@
+import React from "react"
+import UseReducer from "./components/Book.js";
+
+function App(){
+  return <div>
+      <UseReducer/>              
+  </div>
+}
+export default App;
---
+++ React Projects/Booklist/src/components/Book.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react'
+
+const booksData = [
+{id: 1 , name: "Pather Pachali"},   
+{id: 2 , name: "Islamic Gajal"},   
+{id: 3 , name: "Hadis Samagro"},   
+
+];
+
+const UseReducer = () => {
+  const [books, setBooks] = useState(booksData);
+  const [bookName, setBookName] = useState("");
+const handleSubmit = (e) => {
+  e.preventDefault();
+  alert(bookName);
+};
+  
+
+  return (
+    <div>
+      <h3>Book List</h3>
+      <form onSubmit = {handleSubmit}> 
+        <input type="text" value={bookName} 
+        onChange = {(e) => {
+          setBookName(e.target.value);
+        }}
+        />
+        <button type='submit' >Add your Book </button>
+      </form>
+      {books.map((book) => {
+        const { id, name} = book; 
+        return <li key= {id}> {name} </li>
+      })}
+    </div>
+  );
+};
+
+export default UseReducer;
